2c7b13bc296a16e85950a30ffbf37afe8d2de71f
rhedwan <adesolaridwan2003@gmail.com>
2021-04-28 16:56:11 +0100

    first commit

--- CRUDProject/BlogSite/BlogSite/settings.py
+++ CRUDProject/BlogSite/BlogSite/settings.py
@@ -23,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a-=vj80ph*vo7)3dy*q(k8cevj-@f0vl6mk!yl)@b%!&4f_d$l'
+
+SECRET_KEY = "django-insecure-a-=vj80ph*vo7)3dy*q(k8cevj-@f0vl6mk!yl)@b%!&4f_d$l"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -123,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
